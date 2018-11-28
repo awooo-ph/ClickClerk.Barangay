ccf4c0029731c8a926dae2c0581bc2a9c196bc64
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-11-29 05:25:06 +0800

    Update People.cs

--- ClickClerk.Barangay/ViewModels/Pages/People.cs
+++ ClickClerk.Barangay/ViewModels/Pages/People.cs
@@ -1,5 +1,10 @@
-﻿using System.Windows.Data;
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Data;
+using System.Windows.Input;
+using ClickClerk.Barangay.Dialogs;
 using ClickClerk.Barangay.Models;
+using ClickClerk.Barangay.Tools;
 
 namespace ClickClerk.Barangay.ViewModels.Pages
 {
@@ -9,6 +14,7 @@ namespace ClickClerk.Barangay.ViewModels.Pages
         {
             Header = "PEOPLE";
             IsSelected = true;
+            CanSearch = true;
         }
 
         private static People _instnace;
@@ -25,5 +31,38 @@ namespace ClickClerk.Barangay.ViewModels.Pages
                 return _items;
             }
         }
+
+        private ICommand _editEducationCommand;
+
+        public ICommand EditEducationCommand =>
+            _editEducationCommand ?? (_editEducationCommand = new DelegateCommand<Tawo>(async d =>
+            {
+                var educ = new ObservableCollection<Education>(d.Education);
+                var res = await EducationDialog.Show(educ);
+                if (!res) return;
+                foreach (var education in educ)
+                {
+                    education.TawoId = d.Id;
+                    education.Save();
+                    if(!d.Education.Contains(education))
+                        d.Education.Add(education);
+                }
+            }));
+
+        private ICommand _addCommand;
+        public ICommand AddCommand => _addCommand ?? (_addCommand = new DelegateCommand(async d =>
+        {
+            var res = await NewTawo.Show();
+            if (res == null) return;
+            if (res.Data.Save() > 0)
+            {
+                foreach (var education in res.Education)
+                {
+                    education.TawoId = res.Data.Id;
+                    education.Save();
+                    res.Data.Education.Add(education);
+                }
+            }
+        }));
     }
 }
