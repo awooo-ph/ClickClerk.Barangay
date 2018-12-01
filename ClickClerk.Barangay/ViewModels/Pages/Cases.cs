06a5d5a2698f96d62dcb6f56f36777aff21f464c
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-12-01 20:16:33 +0800

    Update Cases.cs

--- ClickClerk.Barangay/ViewModels/Pages/Cases.cs
+++ ClickClerk.Barangay/ViewModels/Pages/Cases.cs
@@ -6,7 +6,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using ClickClerk.Barangay.Dialogs;
+using ClickClerk.Barangay.Models;
 using ClickClerk.Barangay.Tools;
+using MaterialDesignThemes.Wpf;
 
 namespace ClickClerk.Barangay.ViewModels.Pages
 {
@@ -27,11 +29,42 @@ namespace ClickClerk.Barangay.ViewModels.Pages
         public ICommand AddCommand => _addCommand ?? (_addCommand = new DelegateCommand(async d =>
         {
             var res = await CaseDialog.Show();
-            return;
+            
             if (res == null) return;
             if (res.Data.Save() > 0)
             {
+                foreach (var tawo in res.Data.Complainants)
+                {
+                    var complainant = new CaseComplainant()
+                    {
+                        CaseId = res.Data.Id,
+                        TawoId = tawo.Id
+                    };
+                    complainant.Save();
+                }
+
+                foreach (var tawo in res.Data.Respondents)
+                {
+                    new CaseRespondent()
+                    {
+                        CaseId = res.Data.Id,
+                        TawoId = tawo.Id
+                    }.Save();
+                }
                 
+                Items.Add(res.Data);
+            }
+
+        }));
+
+        private ICommand _deleteCommand;
+        public ICommand DeleteCommand => _deleteCommand ?? (_deleteCommand = new DelegateCommand<CaseFile>(async d =>
+        {
+            if (await MessageDialog.Show("Are you sure you want to delete this case?",
+                $"Case number {d.CaseNumber} will be removed from the database. Click DELETE to confirm action.",
+                "DELETE", "CANCEL", PackIconKind.DeleteForever))
+            {
+                d.Delete();
             }
         }));
     }
