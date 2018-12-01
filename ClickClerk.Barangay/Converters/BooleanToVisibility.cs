8a59b0938dc94b7ecc812c4adfdbfadc3caa1b11
awooo-ph <32738756+awooo-ph@users.noreply.github.com>
2018-12-01 20:16:07 +0800

    Create BooleanToVisibility.cs

---
+++ ClickClerk.Barangay/Converters/BooleanToVisibility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClickClerk.Barangay.Converters
+{
+    class BooleanToVisibility:ConverterBase
+    {
+        private BooleanToVisibility(){}
+
+        private static BooleanToVisibility _instance;
+        public static BooleanToVisibility Instance => _instance ?? (_instance = new BooleanToVisibility());
+
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var invert = bool.Parse(parameter?.ToString()??"False");
+            if ((value is bool b && b))
+                return invert ? System.Windows.Visibility.Collapsed : System.Windows.Visibility.Visible;
+            return !invert ? System.Windows.Visibility.Collapsed : System.Windows.Visibility.Visible;
+        }
+    }
+}
